f24af1fbcc77a759ea58432b2d591c29229392a6
corn <ymc.corn@gmail.com>
2026-07-30 23:26:13 +0800

    add ssh TERM setup

--- zsh/.config/zsh/darwin/alias.sh
+++ zsh/.config/zsh/darwin/alias.sh
@@ -1,3 +1,4 @@
+alias ssh='TERM=xterm-256color ssh'
 if command -v bat >/dev/null 2>&1; then
   alias cat="bat"
 fi
--- zsh/.config/zsh/linux/alias.sh
+++ zsh/.config/zsh/linux/alias.sh
@@ -1,4 +1,5 @@
 alias apt="sudo apt"
+alias ssh='TERM=xterm-256color ssh'
 
 if command -v batcat >/dev/null 2>&1; then
   alias cat="batcat"
